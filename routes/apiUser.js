f18b8ff2a524e8f929f2408f1eece5376eb08eda
ighallab <ghallab.a7@gmail.com>
2023-03-20 00:16:42 +0200

    set user endpoints

--- routes/apiUser.js
+++ routes/apiUser.js
@@ -1,11 +1,84 @@
 const express = require('express');
 const router = express.Router();
 
+//require userContorller to interact with databas 
+//and validate the requests body fields
+const userController = require('../controllers/userContorller');
 
 router.get('/',(req,res)=>{
-   res.status(200).json({
-    message:"done indexesxsx"
-   })
+   userController
+      .getAllUser()
+      .then(result=>{
+         res.status(200).json({
+            data: result,
+            status :200,
+            message: "got all data..."
+         })
+      })
+      .catch(err=>{
+         res.status(400).json({
+            data:null,
+            status:400,
+            message:err.message
+         })
+      })
+});
+
+router.get('/:id',(req,res)=>{
+   userController
+      .getUser(req.params)
+      .then(result=>{
+         res.status(200).json({
+            data: result,
+            status :200,
+            message: "got user data..."
+         })
+      })
+      .catch(err=>{
+         res.status(400).json({
+            data:null,
+            status:400,
+            message:err.message
+         })
+      })
+});
+
+router.post('/',(req,res)=>{
+   userController
+      .createUser(req.body)
+      .then(result=>{
+         res.status(200).json({
+            data: result,
+            status :200,
+            message: " user created successfully..."
+         })
+      })
+      .catch(err=>{
+         res.status(400).json({
+            data:null,
+            status:400,
+            message:err.message
+         })
+      })
+});
+
+router.post('/:id/details',(req,res)=>{
+   userController
+      .saveUserDetails(req.params,req.body)
+      .then(result=>{
+         res.status(200).json({
+            data: result,
+            status :200,
+            message: "got user data..."
+         })
+      })
+      .catch(err=>{
+         res.status(400).json({
+            data:null,
+            status:400,
+            message:err.message
+         })
+      })
 });
 
 
