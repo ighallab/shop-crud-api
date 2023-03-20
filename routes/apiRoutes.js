daa2f064a10a6d5a724f9f84d34723fefa2acc7a
ighallab <ghallab.a7@gmail.com>
2023-03-20 22:59:27 +0200

    add product route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mainRouter = express.Router();
 const userRoute = require('./apiUser')
 const categoryRoute = require('./apiCategory');
+const productRoute = require('./apiProduct');
 
 
 mainRouter.use('/user',userRoute);
 mainRouter.use('/category',categoryRoute);
+mainRouter.use('/product',productRoute);
 
 
 module.exports = mainRouter;
