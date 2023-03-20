87c01620f10c551c4b19b130aacb12d5b111bdb6
ighallab <ghallab.a7@gmail.com>
2023-03-20 18:36:54 +0200

    set category route at the mmain route folder

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mainRouter = express.Router();
 const userRoute = require('./apiUser')
+const categoryRoute = require('./apiCategory');
 
 
 mainRouter.use('/user',userRoute);
+mainRouter.use('/category',categoryRoute);
 
 
 module.exports = mainRouter;
