2c7d91747a02838a320bbec13c988d44a6a3e954
ighallab <ghallab.a7@gmail.com>
2023-03-20 17:58:25 +0200

    create product and category relaton N:1

--- models/product.js
+++ models/product.js
@@ -9,20 +9,44 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({Category}) {
       // define association here
+
+      Product.belongsTo(Category,{
+        foreignKey:'categoryId',
+        as:'category'
+      });
+
     }
     
   }
 
   Product.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    price: DataTypes.INTEGER
-  }, {
+
+    title: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    description:{
+      type: DataTypes.STRING,
+      allowNull:false
+    } ,
+    image:{
+      type:DataTypes.STRING
+    },
+    price: {
+      type:DataTypes.INTEGER,
+      allowNull:false
+    }
+
+  },
+   {
+
     sequelize,
     modelName: 'Product',
     tableName:'products'
+  
   });
+
   return Product;
 };
