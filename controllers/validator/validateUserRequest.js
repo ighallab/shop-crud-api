167e1649100fc455638d92a903e0eb1cfe05d9b6
ighallab <ghallab.a7@gmail.com>
2023-03-23 13:34:45 +0200

    do some refactor

--- controllers/validator/validateUserRequest.js
+++ controllers/validator/validateUserRequest.js
@@ -7,16 +7,11 @@ function validatUserRequest(data){
 		id: Joi.number(),
 		username: Joi.string().min(3).max(120).required(),
         password:Joi.string().required(),
-        role:Joi.string().valid('admin','user','guest').required(),
-		userDetails: {
-            
-            first_name: Joi.string(),
-            last_name: Joi.string(),
-		    email: Joi.string().email(),
-            gender:Joi.valid('male','female'),
-            age:Joi.date(),
-            address:Joi.string()
-        }
+        confirm_password: Joi.string()
+            .valid(Joi.ref('password'))
+            .required(),//not working
+        role:Joi.string().valid('admin','user','guest'),
+		token:Joi.string(),
 	
 	});
 
@@ -29,7 +24,9 @@ function validatUserRequestGeneral(data){
 	
 		id: Joi.number(),
 		username: Joi.string().min(3).max(120),
-        password:Joi.string(),
+        password:Joi.string().min(8),
+        confirm_password: Joi.string().valid(Joi.ref('password')),//not working
+		token:Joi.string(),
         role:Joi.string().valid('admin','user','guest'),
 
 		userDetails: {
@@ -46,6 +43,7 @@ function validatUserRequestGeneral(data){
 
 	return schema.validate(data);
 }
+
 function validatUserDetailsRequestGeneral(data){
 
 	const schema = Joi.object({	
@@ -62,8 +60,19 @@ function validatUserDetailsRequestGeneral(data){
 	return schema.validate(data);
 }
 
+function validatePasswordChangeRequest(data){
+    const schema = Joi.object({
+        old_password:Joi.string().required(),
+        new_password:Joi.string().required().min(8),
+        confirm_password: Joi.string().min(8).valid(Joi.ref('new_password')).required()
+    })
+
+    return  schema.validate(data);
+}
+
 module.exports = {
     validatUserRequest,
     validatUserRequestGeneral,
-    validatUserDetailsRequestGeneral
+    validatUserDetailsRequestGeneral,
+    validatePasswordChangeRequest
 };
